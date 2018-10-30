bdd774f9c36f7468290f78d7e8049901f95cedd0
rextao <87639243@qq.com>
2018-10-30 11:09:13 +0800

    设置书签

--- homely/res/js/settingBookmarks.js
+++ homely/res/js/settingBookmarks.js
@@ -3,19 +3,19 @@
  * @Author:RexTao
  * @Date: 2018/10/26
  */
-const SettingBookmarks = function (bookmarks) {
-  this.bookmarks = bookmarks;
+const SettingBookmarks = function (settings, callbacks) {
+  this.settings = settings;
+  this.callbacks = callbacks;
 };
 SettingBookmarks.prototype = {
-  init(style, ctrlDown) {
-    var bookmarksCallbacks = [];
+  init() {
     const that = this;
-    if (this.bookmarks["enable"]) {
+    if (this.settings.bookmarks["enable"]) {
       chrome.permissions.contains({
         permissions: ["bookmarks"]
       }, function (has) {
         if (!has) {
-          that.bookmarks["enable"] = false;
+          that.settings.bookmarks["enable"] = false;
           return;
         }
         // switch to bookmarks page
@@ -26,7 +26,7 @@ SettingBookmarks.prototype = {
           $("#bookmarks").show();
         });
         // show split pane if enabled
-        if (that.bookmarks["split"]) {
+        if (that.settings.bookmarks["split"]) {
           $("#bookmarks-block").before($("<div/>").attr("id", "bookmarks-block-folders").addClass("panel-body"));
           $("#bookmarks-block").before($("<hr/>"));
         }
@@ -38,12 +38,12 @@ SettingBookmarks.prototype = {
           }
           return root;
         };
-        // request tree from Bookmarks API
+        // 请求获取书签
         chrome.bookmarks.getTree(function bookmarksCallback(tree) {
           var root = processBookmarks(tree[0]);
           root.title = "Bookmarks";
-          const bookmarks = new Bookmarks(that.bookmarks,style, ctrlDown,root);
-          const layout = that.bookmarks['layout'];
+          const bookmarks = new Bookmarks(that.settings.bookmarks, that.settings.style, root);
+          const layout = that.settings.bookmarks['layout'];
           switch (layout) {
             case 'folder':
               bookmarks.layoutFolder();
@@ -55,7 +55,7 @@ SettingBookmarks.prototype = {
               bookmarks.layoutDial(6);
               break;
           }
-          if (that.bookmarks["merge"]) {
+          if (that.settings.bookmarks["merge"]) {
             $("#bookmarks").fadeIn();
           } else {
             $("#menu-bookmarks").show();
@@ -64,8 +64,8 @@ SettingBookmarks.prototype = {
           bookmarks.searchEventInit();
         });
         // return any pending callbacks
-        for (var i in bookmarksCallbacks) {
-          bookmarksCallbacks[i].call();
+        for (var i in that.callbacks) {
+          that.callbacks[i].call();
         }
       });
     }
@@ -85,30 +85,30 @@ SettingBookmarks.prototype = {
       }
     });
     // ------配置是否点击了启用按钮
-    $('#settings-bookmarks-enable').prop('checked', this.bookmarks['enable']);
+    $('#settings-bookmarks-enable').prop('checked', this.settings.bookmarks['enable']);
     // ------布局方式
-    $(`.settings-bookmarks-layout-hook input[value=${this.bookmarks['layout']}]`).prop('checked', true);
+    $(`.settings-bookmarks-layout-hook input[value=${this.settings.bookmarks['layout']}]`).prop('checked', true);
 
     //-----------------------文件式布局-----------------
     // 书签显示方式
-    $('#settings-bookmarks-bookmarklets').prop('checked', this.bookmarks['bookmarklets']);
+    $('#settings-bookmarks-bookmarklets').prop('checked', this.settings.bookmarks['bookmarklets']);
     // 文件夹，点击打开下面全部网页
-    $('#settings-bookmarks-foldercontents').prop('checked', this.bookmarks['foldercontents']);
+    $('#settings-bookmarks-foldercontents').prop('checked', this.settings.bookmarks['foldercontents']);
 
-    $('#settings-bookmarks-split').prop('checked', this.bookmarks['split']);
+    $('#settings-bookmarks-split').prop('checked', this.settings.bookmarks['split']);
     // ---------启用书签视图下方的联动
     $('#settings-bookmarks-bookmarklets, #settings-bookmarks-foldercontents, #settings-bookmarks-split, #settings-bookmarks-merge,.settings-bookmarks-layout-hook input')
-      .prop('disabled', !this.bookmarks['enable']).parent().toggleClass('text-muted', !this.bookmarks['enable']);
+      .prop('disabled', !this.settings.bookmarks['enable']).parent().toggleClass('text-muted', !this.settings.bookmarks['enable']);
     // 判断布局方式是否为folder
-    const islayoutFloder = this.bookmarks['layout'] === 'folder';
+    const islayoutFloder = this.settings.bookmarks['layout'] === 'folder';
     $('#settings-bookmarks-layout-folder ').toggleClass('hide', !islayoutFloder);
   },
   save() {
-    this.bookmarks['enable'] = $('#settings-bookmarks-enable').prop('checked');
-    this.bookmarks['layout'] = $('.settings-bookmarks-layout-hook input:checked').prop('value');
-    this.bookmarks['bookmarklets'] = $('#settings-bookmarks-bookmarklets').prop('checked');
-    this.bookmarks['foldercontents'] = $('#settings-bookmarks-foldercontents').prop('checked');
-    this.bookmarks['split'] = $('#settings-bookmarks-split').prop('checked');
+    this.settings.bookmarks['enable'] = $('#settings-bookmarks-enable').prop('checked');
+    this.settings.bookmarks['layout'] = $('.settings-bookmarks-layout-hook input:checked').prop('value');
+    this.settings.bookmarks['bookmarklets'] = $('#settings-bookmarks-bookmarklets').prop('checked');
+    this.settings.bookmarks['foldercontents'] = $('#settings-bookmarks-foldercontents').prop('checked');
+    this.settings.bookmarks['split'] = $('#settings-bookmarks-split').prop('checked');
 
   },
   // setting面板，书签-启用书签视图的checkbox
