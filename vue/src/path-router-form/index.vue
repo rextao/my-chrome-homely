20282d9395883968d4caf6804af511f7b5282409
wangtao05 <wangtao05@kuaishou.com>
2020-06-12 10:18:57 +0800

    增加path转驼峰工具，修改service生成规则

---
+++ vue/src/path-router-form/index.vue
@@ -0,0 +1,62 @@
+<template>
+    <div>
+        <div class="form-wrapper">
+            <el-input
+                    type="textarea"
+                    class="textarea_width"
+                    :rows="5"
+                    :cols="10"
+                    v-model="formData"
+                    @blur="getHumpValue"
+            >
+            </el-input>
+            <el-input
+                    type="textarea"
+                    class="textarea_width"
+                    :rows="5"
+                    :cols="10"
+                    v-model="humpValue"
+                    placeholder="- 转驼峰（用于components）"
+            >
+            </el-input>
+            <el-input
+                    type="textarea"
+                    class="textarea_width"
+                    :rows="5"
+                    :cols="10"
+                    v-model="humpValue2"
+                    placeholder="path转驼峰（用于components）"
+            >
+            </el-input>
+        </div>
+    </div>
+</template>
+<script>
+    export default {
+        name: "index",
+        data() {
+            return {
+                formData: '',
+                humpValue: '',
+            };
+        },
+        methods: {
+             toHump(name) {
+                return name.replace(/-(\w)/g, (all, letter) => letter.toUpperCase());
+            },
+            getHumpValue() {
+                 this.humpValue = this.formData.replace(/^[a-z]/, (l) => l.toUpperCase()).replace(/-(\w)/g, (all, letter) => letter.toUpperCase());
+                 this.humpValue2 = this.formData.replace(/\/(\w)/g, (all, letter) => letter.toUpperCase());
+            }
+        },
+    }
+</script>
+<style scoped>
+    .form-wrapper {
+        display: flex;
+    }
+    .textarea_width {
+        width: 400px;
+        margin-right: 10px;
+    }
+</style>
