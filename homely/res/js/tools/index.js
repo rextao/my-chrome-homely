3d12b122f9e948281dbd7aaa79926b3a561ea36c
rextao <87639243@qq.com>
2021-07-20 10:30:45 +0800

    add 新增 直接转换为新service功能 + 旧service转为新service功能

--- homely/res/js/tools/index.js
+++ homely/res/js/tools/index.js
@@ -2,12 +2,16 @@
     const $original = $('#original');
     const $const = $('#const');
     const $service = $('#service');
+    const $serviceNew = $('#serviceNew');
     const $pre = $('#pre');
     const $actionPre = $('#actionPre');
     // 默认值
     $actionPre.val('/v4/operation/');
     $('#btn').on('click', function () {
         create();
+    });
+    $('#newBtn').on('click', function () {
+        createNewService();
     });
     $('#clear').on('click', function () {
         clear();
@@ -26,6 +30,7 @@
         const num = actionPre.length;
         const action = [];
         const service = [];
+        let newService = '';
         originalArray.forEach(item => {
             // 将文本多余字符串，尤其是删除注释
             const url = item.replace(/[\u4e00-\u9fa5].*/g, '').replace(/[\s，,]*/g, '');
@@ -42,9 +47,30 @@
             const upperKey = `${preUrl}${lineUrl}`;
             service.push(`${key}: '${upperKey}'`);
             action.push(`${upperKey}: '${url}'`);
+            newService += getFunc(key, upperKey);
         });
         $const.val(action.join(',\n'));
         $service.val(service.join(',\n'));
+        $serviceNew.val(newService);
+    }
+    // 主要是为了从旧service转为新的
+    function createNewService() {
+        const oldServiceVal = $service.val();
+        const arr = oldServiceVal.split(',');
+        let result = '';
+        arr && arr.forEach(item => {
+            const [key, val] = item.split(':');
+            if (val) {
+                result += getFunc(key, val)
+            }
+        });
+        $serviceNew.val(result);
+    }
+    function getFunc(name, url) {
+        const convertUrl = url.trim().replace(/'/g, '');
+        return `${name}(payload) {
+                    return $http.post(URL_API.${convertUrl}, payload);
+                },\n`
     }
     function toLine(name) {
         return name.replace(/([A-Z])/g,"_$1").toLowerCase();
